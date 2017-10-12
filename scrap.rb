98fa794bbc5142bdafc32cdab5265fd14a6e3713
Tarek Dinkespiler <tarek.dinkespiler@gmail.com>
2017-10-12 15:31:17 +0200

    178 mails :/

--- scrap.rb
+++ scrap.rb
@@ -1,20 +1,44 @@
 require 'rubygems'
-require 'nokogiri'   
+require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 
-page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/"))   
-puts page.class   # => Nokogiri::HTML::Document
 
-link_array = []
-page.css("a[href]").each do |link|
-	link_array << link.to_s
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+
+def open_link
+
+  scrapped = []
+
+  landing_page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))   
+
+  links = []
+  landing_page.css('a[class = lientxt]').each do |link|
+	link_end = link["href"].sub!(".","http://annuaire-des-mairies.com")
+	links << link_end
+
+  end
+
+links.each do |ville|
+  page = Nokogiri::HTML(open("#{ville}"))   
+
+  text_array = page.css('td[class = style27]').text.split
+  mail = []
+
+  text_array.each do |element|
+    #mail << element if element.include?("@")
+    scrapped << element if element.include?("@")
+  end
+end 
+  puts scrapped
+  puts scrapped.size
+  puts links.size
+
 end
 
-mail_array = []
-link_array.each do |mail|
-	mail_array << mail if mail.include?("mailto") && mail.include?("@")
-end
-
-puts mail_array
-
+open_link
